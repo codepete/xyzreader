0e2b0c13b0dd6a7c1d4c79e23d74ecf9ac47fff7
Peter Ung <ung.peter@gmail.com>
2016-04-13 14:00:11 -0700

    Adding source

---
+++ XYZReader/src/main/java/com/example/xyzreader/custom/CustomImageView.java
@@ -0,0 +1,18 @@
+package com.example.xyzreader.custom;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by peter on 4/8/16.
+ */
+public class CustomImageView extends ImageView {
+    public CustomImageView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+    }
+}
